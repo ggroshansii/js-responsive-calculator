f8b1be202fe3bfe76b66e77af9eeef7fa62178ea
ggroshansii <garthgroshansii@gmail.com>
2021-09-02 13:57:23 -0400

    defined a function called pushNumber() that's nested inside a 'click' eventlistener; pushNumber() uses the alert() function to display the number button clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-alert("You're page is linked!");
+const numberBtns = document.querySelectorAll(".number");
+
+
+function pushNumber(element) {
+    alert(`You have clicked number ${element}`)
+}
+
+for (let i=0; i<numberBtns.length; i++) {
+    console.log(numberBtns[i]);
+    numberBtns[i].addEventListener('click', ()=> {
+        pushNumber(numberBtns[i].value);
+    });
+}
