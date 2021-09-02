edca46ee138e0e958546cf704c4ae34b9385f0bb
ggroshansii <garthgroshansii@gmail.com>
2021-09-02 14:05:45 -0400

    added pushOperator() function that's nested inside a 'click' eventlistener; pushOperator() alert's the operator that is clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
+
+//selecting groups of buttons (numbers, operators)
 const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
 
-
-function pushNumber(element) {
-    alert(`You have clicked number ${element}`)
+//alerts the user of the number that was 'clicked'; Nested inside a 'click' eventlistener
+function pushNumber(numElem) {
+    alert(`You have clicked number ${numElem}`)
 }
 
+//loops through the number buttons nodelist (.number) and attaches 'click' eventlistener to each node
 for (let i=0; i<numberBtns.length; i++) {
-    console.log(numberBtns[i]);
-    numberBtns[i].addEventListener('click', ()=> {
+    numberBtns[i].addEventListener('click', () => {
         pushNumber(numberBtns[i].value);
     });
+}
+
+//alerts the user of the operator that was 'clicked'; Nested inside a 'click' eventlistener
+function pushOperator(operElem) {
+    alert(`You have clicked the ${operElem} operator`);
+}
+
+//loops through the operator buttons nodelist (.operator) and attaches 'click' eventlistener to each node
+for (let i=0; i<operatorBtns.length; i++) {
+    operatorBtns[i].addEventListener('click', () => {
+        pushOperator(operatorBtns[i].value);
+    })
 }
