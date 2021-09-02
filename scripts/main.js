b88e4c16fb6b38a40b72ca870019dd190e564097
ggroshansii <garthgroshansii@gmail.com>
2021-09-02 16:45:24 -0400

    added regex that will allow the string expression to be split based on operators

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 
-//selecting groups of buttons (numbers, operators)
+//selecting specific groups of buttons (numbers, operators)
 const numberBtns = document.querySelectorAll(".number");
 const operatorBtns = document.querySelectorAll(".operator");
-const equalBtn = document.querySelector(".equal-sign");
+const equalsBtn = document.querySelector(".equal-sign");
 
+//array dynamically holding the current expression
 let calculation = []; 
 
 //alerts the user of the number that is 'clicked'; Nested inside a 'click' eventlistener
 function pushNumber(numElem) {
-    alert(`You have clicked number ${numElem}`)
+    //alert(`You have clicked number ${numElem}`)
+    calculation.push(numElem);
 }
 
 //loops through the number buttons nodelist (.number) and attaches 'click' eventlistener to each node
@@ -20,7 +22,8 @@ for (let i=0; i<numberBtns.length; i++) {
 
 //alerts the user of the operator that is 'clicked'; Nested inside a 'click' eventlistener
 function pushOperator(operElem) {
-    alert(`You have clicked the ${operElem} operator`);
+    //alert(`You have clicked the ${operElem} operator`);
+    calculation.push(operElem);
 }
 
 //loops through the operator buttons nodelist (.operator) and attaches 'click' eventlistener to each node
@@ -30,11 +33,35 @@ for (let i=0; i<operatorBtns.length; i++) {
     })
 }
 
-//alerts the user when the equals sign is 'clicked'; Nested inside a 'click' eventlistener
-function calculate(equalsElem) { 
-    alert(`The ${equalsElem} sign was pressed`);
+//addition function
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
 }
 
-equalBtn.addEventListener('click', () => {
-    calculate(equalBtn.value);
-})
+//subtraction function
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+//multiplication function
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+//division function
+function division(num1, num2) {
+    return Number(num1) / Number(num2);
+}
+
+
+//alerts the user when the equals sign is 'clicked'; Nested inside a 'click' eventlistener
+function calculate(equalsElem) { 
+    //alert(`The ${equalsElem} sign was pressed`);
+
+//console.log(str.split(/\+|-|\*|\//g));
+}
+
+//attaches a click eventlistener to the equalsBtn
+equalsBtn.addEventListener('click', () => {
+    calculate(equalsBtn.value);
+});
