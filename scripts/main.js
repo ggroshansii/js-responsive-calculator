5eebf07779e5755f515fddab74aa7cf38e541060
ggroshansii <garthgroshansii@gmail.com>
2021-09-05 21:55:54 -0400

    renamed file to be the main js script file

--- scripts/main.js
+++ scripts/main.js
@@ -10,28 +10,38 @@ const plusMinusBtn = document.querySelector(".plus-minus");
 
 //array dynamically holding the current expression
 let calculation = [];
-let a;
+let accum = null;
+let lastNumPressed = null;
+let lastOpPressed = null;
+let calcRunning = true;
 
 //alerts the user of the number that is 'clicked'; Nested inside a 'click' eventlistener
 function pushNumber(numElem) {
-    alert(`You have clicked number ${numElem}`);
+    //alert(`You have clicked number ${numElem}`);
     calculation.push(numElem);
 }
 
 //alerts the user of the operator that is 'clicked'; Nested inside a 'click' eventlistener
 function pushOperator(operElem) {
-    alert(`You have clicked the ${operElem} operator`);
+    //alert(`You have clicked the ${operElem} operator`);
     calculation.push(operElem);
 }
 
 //LISTENER: loops through the number buttons nodelist (.number) and attaches 'click' eventlistener to each node
 for (let i = 0; i < numberBtns.length; i++) {
     numberBtns[i].addEventListener("click", () => {
-        pushNumber(numberBtns[i].value);
-        if ("+-/*".includes(calculation[calculation.length - 2])) {
-            calculatorScreen.value = numberBtns[i].value;
-        } else {
-            calculatorScreen.value += numberBtns[i].value;
+        if (calcRunning) {
+            if (
+                lastNumPressed !== null &&
+                ".0123456789".includes(calculation[calculation.length - 1])
+            ) {
+                lastNumPressed += numberBtns[i].value;
+                calculatorScreen.value = lastNumPressed;
+            } else {
+                lastNumPressed = numberBtns[i].value;
+                calculatorScreen.value = lastNumPressed;
+            }
+            pushNumber(numberBtns[i].value);
         }
     });
 }
@@ -39,123 +49,144 @@ for (let i = 0; i < numberBtns.length; i++) {
 //LISTENER: loops through the operator buttons nodelist (.operator) and attaches 'click' eventlistener to each node
 for (let i = 0; i < operatorBtns.length; i++) {
     operatorBtns[i].addEventListener("click", () => {
-        pushOperator(operatorBtns[i].value);
-        calculatorScreen.value = operatorBtns[i].value;
-        calculate();
+        if (calcRunning) {
+            pushOperator(operatorBtns[i].value);
+            calculatorScreen.value = operatorBtns[i].value;
+            calculate();
+            lastOpPressed = operatorBtns[i].value;
+        }
     });
 }
 
 //LISTENER: attaches a click eventlistener to the equals button
 equalsBtn.addEventListener("click", () => {
-    calculation.push("=");
-    calculate(equalsBtn.value);
+    if (calcRunning) {
+        calculation.push("=");
+        calculate(equalsBtn.value);
+    }
 });
 
 //LISTENER: attaches click event listener to the clear button
 clearBtn.addEventListener("click", () => {
     calculation = [];
-    a = undefined;
+    accum = null;
+    lastNumPressed = null;
+    lastOpPressed = null;
+    calcRunning = true;
     calculatorScreen.value = "";
 });
 
-//LISTENER: attaches click event listener to decimal button
+// //LISTENER: attaches click event listener to decimal button
 decimalBtn.addEventListener("click", () => {
-    calculation.push(".");
-    calculatorScreen.value += ".";
+    if (calcRunning) {
+        calculation.push(".");
+        calculatorScreen.value += ".";
+        lastNumPressed += ".";
+    }
+
 });
 
 //LISTENER: attaches click event listener to decimal button
 percentageBtn.addEventListener("click", () => {
-    let splitNumbers = calculation
-        .join("")
-        .split(/\+|-|\*|\//g)
-        .filter((x) => !!x);
-    addPercentNum = splitNumbers.pop();
-    for (let i = calculation.length - 1; i >= 0; i--) {
-        if (String(addPercentNum).includes(calculation[i])) {
-            calculation.splice(i, 1);
+    if (calcRunning) {
+        let currentNumLength = lastNumPressed.length;
+        calculation.splice(
+            calculation.length - currentNumLength,
+            currentNumLength
+        );
+        lastNumPressed = String(lastNumPressed / 100);
+        calculation.push(lastNumPressed.split(""));
+        calculatorScreen.value = lastNumPressed;
+    }
+});
+
+//LISTENER: attaches click event listener to the plus/minus button
+plusMinusBtn.addEventListener("click", () => {
+    if (calcRunning) {
+        if (lastNumPressed.includes("-")) {
+            lastNumPressed = lastNumPressed.slice(1);
+            let currentNumLength = lastNumPressed.length;
+            calculation.splice(
+                calculation.length - currentNumLength,
+                currentNumLength
+            );
+            calculation.push(lastNumPressed.split(""));
+            calculatorScreen.value = lastNumPressed;
+        } else {
+            let currentNumLength = lastNumPressed.length;
+            calculation.splice(
+                calculation.length - currentNumLength,
+                currentNumLength
+            );
+            lastNumPressed = "-" + lastNumPressed;
+            calculation.push(lastNumPressed.split(""));
+            calculatorScreen.value = lastNumPressed;
         }
     }
-    calculation.push(addPercentNum / 100);
-    calculatorScreen.value = addPercentNum / 100;
 });
 
 //addition function
 function add(num1, num2) {
-    calculation = calculation.slice(-1);
     return Number(num1) + Number(num2);
 }
 
 //subtraction function
 function subtract(num1, num2) {
-    calculation = calculation.slice(-1);
     return Number(num1) - Number(num2);
 }
 
 //multiplication function
 function multiply(num1, num2) {
-    calculation = calculation.slice(-1);
     return Number(num1) * Number(num2);
 }
 
 //division function
 function divide(num1, num2) {
-    calculation = calculation.slice(-1);
     return Number(num1) / Number(num2);
 }
 
-//alerts the user when the equals sign is 'clicked'; Nested inside a 'click' eventlistener
+//calculates the accumulated variable (accum) with the next operator and number
 function calculate(equalsElem = null) {
     //for looping checking if '=' was added to calculation array
     for (let i = 0; i < calculation.length; i++) {
         if (calculation[i] === "=") {
+            calcRunning = false;
             alert(`The ${equalsElem} sign was pressed`);
-            calculation.pop();
+            findOperator();
+            calculatorScreen.value = accum;
+            calculation = [];
+            calculation.push(accum);
+            return;
         }
     }
-
-    //regex to breakdown calculation array into two separate arrays: numbers & operators
-    let splitNumbers = calculation
-        .join("")
-        .split(/\+|-|\*|\//g)
-        .filter((x) => !!x);
-    let splitOperator = calculation
-        .join("")
-        .split(/[0-9\.]+/)
-        .filter((x) => !!x);
-
     //checking to see if calculate() needs to set the very first number entered; after first number is entered, the rest of the operators will be between an accumulator variable, the newest operator selected and a second number
-    if (a === undefined) {
-        a = Number(splitNumbers[0]);
-        splitNumbers = [];
-        //This loop is going to take off the first number recieved -- loop needed for multi-digit numbers
-        for (let i = calculation.length - 1; i >= 0; i--) {
-            if ("0123456789.".includes(calculation[i])) {
-                calculation.splice(i, 1);
-            }
-        }
+    if (accum === null) {
+        accum = lastNumPressed;
+        console.log("accum now", accum);
         return;
     } else {
-        //switch case for finding which operator function to fire
-        switch (splitOperator[0]) {
-            case "+":
-                a = add(a, splitNumbers[0]);
-                calculatorScreen.value = a;
-                break;
-            case "-":
-                a = subtract(a, splitNumbers[0]);
-                calculatorScreen.value = a;
-                break;
+        findOperator();
+    }
+}
 
-            case "*":
-                a = multiply(a, splitNumbers[0]);
-                calculatorScreen.value = a;
-                break;
-
-            case "/":
-                a = divide(a, splitNumbers[0]);
-                calculatorScreen.value = a;
-                break;
-        }
+//finds value of lastOpPressed and executes corresponding operator function
+function findOperator() {
+    switch (lastOpPressed) {
+        case "+":
+            accum = add(accum, lastNumPressed);
+            calculatorScreen.value = accum;
+            break;
+        case "-":
+            accum = subtract(accum, lastNumPressed);
+            calculatorScreen.value = accum;
+            break;
+        case "*":
+            accum = multiply(accum, lastNumPressed);
+            calculatorScreen.value = accum;
+            break;
+        case "/":
+            accum = divide(accum, lastNumPressed);
+            calculatorScreen.value = accum;
+            break;
     }
 }
